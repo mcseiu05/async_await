18c978d82c2cee6bb74d2a453f6a1ebba8f51106
Mamun Ahammed <m.cseiu05@gmail.com>
2019-11-30 20:59:27 +0600

    First commit

---
+++ Async/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Async
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Process File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(134, 62);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 13);
+            this.lblMsg.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 427);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Async and await";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
---
+++ Async/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Async
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private long CountCharecters()
+        {
+            long count = 0;
+            
+            using(StreamReader reader=new StreamReader("E:\\Practice\\Async.txt"))
+            {
+                String content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Task<long> task = new Task<long>(CountCharecters);
+            task.Start();
+            lblMsg.Text = "Processing file. Please wait..";
+            long c = await task;
+            lblMsg.Text = "There are " + c.ToString() + " charecters in the file."; 
+        }
+    }
+}
